bfc7e8b20285d830d0f639f88c1235050cec10c5
RicardoAscF <ricardoascencio3.14@gmail.com>
2024-03-16 14:20:51 -0600

    Select Added properly

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 
+  
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+});
+
+
+
 function addTask(){
     alert('click en Add task');
 }
