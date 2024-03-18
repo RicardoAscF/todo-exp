c8b3972e9e055694336bfff8ecf117d052c53771
RicardoAscF <ricardoascencio3.14@gmail.com>
2024-03-17 22:09:33 -0600

    Inserciones en la BD

--- js/app.js
+++ js/app.js
@@ -1,154 +1,182 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
+
+
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+ // Your web app's Firebase configuration
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ const firebaseConfig = {
+     apiKey: "AIzaSyDKjmJ28jrpzJ7MF5tgFGXfYBI65BQP2Dc",
+     authDomain: "todo-exp-b768a.firebaseapp.com",
+     projectId: "todo-exp-b768a",
+     storageBucket: "todo-exp-b768a.appspot.com",
+     messagingSenderId: "1069309297345",
+     appId: "1:1069309297345:web:fbcf23ac219884faea8a40",
+     measurementId: "G-Z5DGDQ95TY"
+ };
+
+   // Initialize Firebase
+   const app = initializeApp(firebaseConfig);
+   const analytics = getAnalytics(app);
+   firebase.initializeApp(firebaseConfig);
+   const db = firebase.firestore();
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
 });
 
 
+const addTaskBtn = document.getElementById('addTaskButton');
+addTaskBtn.addEventListener('click', ()=>{
+    addTask();
+});
 
 function addTask(){
-    id              =   Date.now();
-    taskName        =   document.getElementById('icon_prefix').value;
-    exp             =   document.getElementById('icon_time-exp').value;
-    selectedIcon    =   document.getElementById('selectedIcon').value;
-    alert(selectedIcon);
+    let id              =   Date.now();
+    let taskName        =   document.getElementById('icon_prefix').value;
+    let exp             =   document.getElementById('icon_time-exp').value;
+    let selectedIcon    =   document.getElementById('selectedIcon').value;
+   
     fillTasksTobeDone(id,taskName,exp,selectedIcon);
+    
+    //FuncionInsertar en la BD
 
+    insertDB(id,taskName,exp,selectedIcon);
+    document.getElementById('icon_prefix').value='';
+    document.getElementById('icon_time-exp').value='';
 }//AddTask
+
+
+async function insertDB(id,taskName,exp,selectedIcon){
+    db.collection("tasks").add({
+        id: id,
+        taskName: taskName,
+        exp: exp,
+        selectedIcon, selectedIcon
+    })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+}
+
+
 
 
 function fillTasksTobeDone(id,taskName,exp,selectedIcon){
 
-let taskToBeDone            = document.getElementById('tasksToBeDone');
+    let taskToBeDone            = document.getElementById('tasksToBeDone');
 
 
-let divColS12M4             = document.createElement('div');
-divColS12M4.classList.add('col', 's12', 'm4');
-
-
-
-let divIcon_BlockCard_Task  = document.createElement('div');
-divIcon_BlockCard_Task.classList.add('icon-block', 'card-task');   
+    let divColS12M4             = document.createElement('div');
+    divColS12M4.classList.add('col', 's12', 'm4');
 
 
 
-let h2CenterLight_blue_text = document.createElement('h2');
-h2CenterLight_blue_text.classList.add('center', 'light-blue-text');
-
-let iMaterial_icons         = document.createElement('i');
-iMaterial_icons.classList.add('material-icons');
+    let divIcon_BlockCard_Task  = document.createElement('div');
+    divIcon_BlockCard_Task.classList.add('icon-block', 'card-task');   
 
 
 
+    let h2CenterLight_blue_text = document.createElement('h2');
+    h2CenterLight_blue_text.classList.add('center', 'light-blue-text');
 
-/*
-
-<option value=""  selected>It is for</option>
-                <option value="1">Cooking</option>
-                <option value="2">Exersice</option>
-                <option value="3">Having Fun</option>
-                <option value="4">Personal Projects</option>
-                <option value="5">Shopping</option>
-                <option value="6">Job</option>
-* */
+    let iMaterial_icons         = document.createElement('i');
+    iMaterial_icons.classList.add('material-icons');
 
 
-let icon='';
+    let icon='';
 
-if(selectedIcon == '1'){
-    icon='local_pizza';
-}
-
-
-if(selectedIcon == '2'){
-    icon='directions_bike';
-}
+    if(selectedIcon == '1'){
+        icon='local_pizza';
+    }
 
 
-if(selectedIcon == '3'){
-    icon='insert_emoticon';
-}
+    if(selectedIcon == '2'){
+        icon='directions_bike';
+    }
 
 
-if(selectedIcon == '4'){
-    icon='accessibility';
-}
-
-if(selectedIcon == '5'){
-    icon='shopping_cart';
-}
+    if(selectedIcon == '3'){
+        icon='insert_emoticon';
+    }
 
 
-if(selectedIcon == '6'){
-   icon='attach_money';
-}
+    if(selectedIcon == '4'){
+        icon='accessibility';
+    }
+
+    if(selectedIcon == '5'){
+        icon='shopping_cart';
+    }
+
+
+    if(selectedIcon == '6'){
+    icon='attach_money';
+    }
 
 
 
 
-alert(`Icon ${icon}`);
-iMaterial_icons.innerText=icon;//AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+
+    iMaterial_icons.innerText=icon;//AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+
+    let h5CenterTitles          = document.createElement('h5');
+    h5CenterTitles.classList.add('center', 'titles');
+    h5CenterTitles.innerText=taskName;
+
+    let divCard_Task_Buttons    = document.createElement('div');
+    divCard_Task_Buttons.classList.add('card-task-buttons');
+
+    let aDone                   = document.createElement('a');
+    aDone.classList.add('waves-effect','waves-light','btn-small');
+    aDone.innerText='Done';
+
+    let iDone = document.createElement('i');
+    iDone.classList.add('material-icons', 'right');
+    iDone.innerText='check_box';
+
+
+    let aCancel                 = document.createElement('a');
+    aCancel.classList.add('waves-effect','waves-light','btn-small', 'red');
+    aCancel.innerText='Cancel';
+
+    let iCancel = document.createElement('i');
+    iCancel.classList.add('material-icons', 'right');
+    iCancel.innerText = 'cancel';
+
+    let p                       = document.createElement('p');
+    p.classList.add('light', 'tektur');
+    p.innerText = `${exp} EXP`; //AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
 
 
 
-let h5CenterTitles          = document.createElement('h5');
-h5CenterTitles.classList.add('center', 'tiltes');
-h5CenterTitles.innerText=taskName;
+    //////////////////////////////  Agrega los 'i' a los 'a'
+    aDone.appendChild(iDone);
+    aCancel.appendChild(iCancel);
+
+    divCard_Task_Buttons.appendChild(aDone);
+    divCard_Task_Buttons.appendChild(aCancel);
+
+    h2CenterLight_blue_text.appendChild(iMaterial_icons);
+
+    divIcon_BlockCard_Task.appendChild(h2CenterLight_blue_text);
+    divIcon_BlockCard_Task.appendChild(h5CenterTitles);
+    divIcon_BlockCard_Task.appendChild(divCard_Task_Buttons);
+    divIcon_BlockCard_Task.appendChild(p);
 
 
+    divColS12M4.appendChild(divIcon_BlockCard_Task);
 
-
-let divCard_Task_Buttons    = document.createElement('div');
-divCard_Task_Buttons.classList.add('card-task-buttons');
-
-let aDone                   = document.createElement('a');
-aDone.classList.add('waves-effect','waves-light','btn-small');
-aDone.innerText='Done';
-
-let iDone = document.createElement('i');
-iDone.classList.add('material-icons', 'right');
-iDone.innerText='check_box';
-
-
-let aCancel                 = document.createElement('a');
-aCancel.classList.add('waves-effect','waves-light','btn-small', 'red');
-aCancel.innerText='Cancel';
-
-let iCancel = document.createElement('i');
-iCancel.classList.add('material-icons', 'right');
-iCancel.innerText = 'cancel';
-
-let p                       = document.createElement('p');
-p.classList.add('light', 'tektur');
-p.innerText = '10 PX'; //AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
-
-
-
-//////////////////////////////  Agrega los 'i' a los 'a'
-aDone.appendChild(iDone);
-aCancel.appendChild(iCancel);
-
-divCard_Task_Buttons.appendChild(aDone);
-divCard_Task_Buttons.appendChild(aCancel);
-
-h2CenterLight_blue_text.appendChild(iMaterial_icons);
-
-divIcon_BlockCard_Task.appendChild(h2CenterLight_blue_text);
-divIcon_BlockCard_Task.appendChild(h5CenterTitles);
-divIcon_BlockCard_Task.appendChild(divCard_Task_Buttons);
-divIcon_BlockCard_Task.appendChild(p);
-
-
-divColS12M4.appendChild(divIcon_BlockCard_Task);
-
-taskToBeDone.appendChild(divColS12M4);
-
-
-
-
+    taskToBeDone.appendChild(divColS12M4);
 }//fillTasksTobeDone
 
 
 
-
-
-console.log(Date.now());
